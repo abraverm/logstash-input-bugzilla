12ef7d80fa6bb2087b9294ce74bcc41e527e9489
Alexander Braverman <abraverm@redhat.com>
2015-03-21 07:13:09 -0400

    Initial plugin based on logstash-input-example.
    Change-Id: I681669fc023c4c1c5af52eacf6a8bc0a74e04f86
    Signed-off-by: Alexander Braverman <abraverm@redhat.com>

---
+++ spec/inputs/bugzilla_spec.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+# Add the gem 'logstash-devutils' to Logstash Gemfile (not plugin's Gemfile)
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/inputs/bugzilla'
+require 'bugzilla/bug'
+require 'bugzilla/plugin'
+require 'bugzilla/product'
+require 'bugzilla/user'
+require 'bugzilla/xmlrpc'
+require 'bugzilla/bugzilla'
+
+describe "inputs/bugzilla" do
+
+  context  "registration" do
+    it "can register and tear down with default configurations" do
+      input = LogStash::Plugin.lookup("input", "bugzilla").new
+      expect { input.register }.to_not raise_error
+      expect { input.teardown }.to_not raise_error
+    end
+  end
+
+
+end
