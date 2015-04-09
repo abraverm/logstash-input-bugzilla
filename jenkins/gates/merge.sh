e7c8dc49ef63ea2cffbf8653c9a221b6ebb78c02
Alexander Braverman <abraverm@redhat.com>
2015-04-09 20:30:35 +0300

    More Docker and Ruby gate
    Change-Id: I293e7e99bda9b7b8d7b1c944d5db870c15955071

---
+++ jenkins/gates/merge.sh
@@ -0,0 +1,24 @@
+ set +x
+ echo "====================="
+ echo "Checking Commit"
+ echo "====================="
+ echo "==== Trying merge to master ===="
+ pushd "$WORKSPACE/logstash-input-bugzilla_${BUILD_NUMBER}"
+ # store the checked-out refspec in a branch
+ git checkout -b thechange
+ # switch to branch
+ git checkout ci
+ # and merge or fail
+ if ! git merge --commit -m 'Merge the gated change' thechange; then
+   echo -e "\033[01;31m===> This change has to be rebased!\033[00m"
+   echo ""
+   exit 1
+ fi
+ echo " - merge to master OK"
+ echo "====================="
+ echo ""
+ echo "changes:"
+ if [ "$GERRIT_REFSPEC" != "refs/heads/ci" ]; then
+   git diff HEAD^..HEAD
+ fi
+ exit 0
